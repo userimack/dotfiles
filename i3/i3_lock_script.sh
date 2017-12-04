07c65e2455be553935cb2a7a5b5670c524732811
Mahendra Yadav <mahendra@azrisolutions.com>
2017-12-04 11:19:28 +0530

    Modified script

--- i3/i3_lock_script.sh
+++ i3/i3_lock_script.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 revert() {
   xset dpms 0 0 0
 }
 trap revert HUP INT TERM
 xset +dpms dpms 5 5 5
 # i3lock -n
-i3lock --image=/home/mahendra/Pictures/wallpaper/IMG_20171126_175649.png --tiling --pointer=default --ignore-empty-password --show-failed-attempts -n
+xrandr | grep -e 'HDMI1 connected' -e 'HDMI2 connected';
+if (( $? == 0 ))
+then
+    i3lock --image=/home/mahendra/Pictures/wallpaper/screenlock_image.png --tiling --pointer=default --ignore-empty-password --show-failed-attempts -n
+else
+    i3lock --image=/home/mahendra/Pictures/wallpaper/IMG_20171126_175649.png --tiling --pointer=default --ignore-empty-password --show-failed-attempts -n
+fi
+
 revert
